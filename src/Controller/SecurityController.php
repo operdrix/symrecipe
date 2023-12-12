dcf440020b8ea0491688fb838888ca93aa4603a3
Olivier PERDRIX <olivierperdrix@live.fr>
2023-12-12 12:55:36 +0100

    ✨add login page

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $auth): Response
+    {
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $auth->getLastUsername(),
+            'error' => $auth->getLastAuthenticationError(),
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'security.logout', methods: ['GET'])]
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
