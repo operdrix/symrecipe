29d725fbb6bf3cf7202bf5ca7f38eacdf7299bdf
Olivier PERDRIX <olivierperdrix@live.fr>
2023-12-12 19:35:40 +0100

    ✨Add User Password edit page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserPasswordType;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -54,8 +55,53 @@ class UserController extends AbstractController
             }
         }
 
-
         return $this->render('pages/user/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Cette route permet d'afficher le formulaire de modification du mot de passe
+     * 
+     * @Route("/utilisateur/mot-de-passe/{id}", name="user.edit.password")
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/utilisateur/mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
+    public function editPassword(
+        User $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher
+    ): Response {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('security.login');
+        }
+
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setPassword($hasher->hashPassword($user, $form->getData()['newPassword']));
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash('success', 'Votre mot de passe a bien été modifié.');
+
+                return $this->redirectToRoute('recipe.index');
+            } else {
+                $this->addFlash('warning', 'Le mot de passe actuel est incorrect.');
+            }
+        }
+
+        return $this->render('pages/user/edit.password.html.twig', [
             'form' => $form->createView(),
         ]);
     }
